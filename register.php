1334ecc5413a2ab0ca51acc9da332673dd43848f
Benjamin Oldén <berntsson90@hotmail.com>
2020-10-06 22:17:46 +0200

    clean up register and login form

---
+++ register.php
@@ -0,0 +1,121 @@
+<?php 
+
+function sanitizeFormPassword($inputText) {
+
+    $inputText = strip_tags($inputText);
+
+    return $inputText;
+}
+
+function sanitizeFormUsername($inputText) {
+
+    $inputText = strip_tags($inputText);
+    $inputText = str_replace(" ", "", $inputText);
+
+    return $inputText;
+}
+
+function sanitizeFormString($inputText) {
+
+    $inputText = strip_tags($inputText);
+    $inputText = str_replace(" ", "", $inputText);
+    $inputText = ucfirst(strtolower($inputText));
+
+    return $inputText;
+}
+
+if(isset($_POST['loginButton'])) {
+    //Login button was pressed 
+}
+
+if(isset($_POST['registerButton'])) {
+    //Register button was pressed 
+    $username = sanitizeFormUsername($_POST['username']);
+    
+    $firstName = sanitizeFormString($_POST['firstName']);
+
+    $lastName = sanitizeFormString($_POST['lastName']);
+    
+    $email = sanitizeFormString($_POST['email']);
+
+    $email2 = sanitizeFormString($_POST['email2']);
+  
+    $password = sanitizeFormPassword($_POST['password']);
+    
+    $password2 = sanitizeFormPassword($_POST['password2']);
+}
+
+
+?>
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to Spotify Clone</title>
+</head>
+<body>
+    <div id="inputContainer">
+        <form id="loginForm" action="register.php" method="POST">
+            <h2>Login to your account</h2>
+            <p>
+            <label for="loginUsername">Username</label>
+            <input id="loginUsername" name="loginUsername" type="text" placeholder="e.g. Username" required>
+            </p>
+            <p>
+            <label for="loginPassword">Password</label>
+            <input id="loginPassword" name="loginPassword" type="password"  placeholder="Your Password" required>
+            </p>
+
+            <button type="submit" name="loginButton">Login</button>
+        </form>
+
+
+        <form id="registerForm" action="register.php" method="POST">
+            <h2>Create your account</h2>
+            <p>
+            <label for="username">Username</label>
+            <input id="username" name="username" type="text" placeholder="e.g. Username" required>
+            </p>
+            <p>
+            <label for="firstName">First name</label>
+            <input id="firstName" name="firstName" type="text" placeholder="e.g. Firstname" required>
+            </p>
+
+            <p>
+            <label for="lastName">lastName</label>
+            <input id="lastName" name="lastName" type="text" placeholder="e.g. LastName" required>
+            </p>
+
+            <p>
+            <label for="email">Email</label>
+            <input id="email" name="email" type="email" placeholder="e.g. spotifyclone@hotmail.com" required>
+            </p>
+
+            <p>
+            <label for="email2">Confirm email</label>
+            <input id="email2" name="email2" type="email" placeholder="e.g. spotifyclone@hotmail.com" required>
+            </p>
+
+
+            <p>
+            <label for="password">Password</label>
+            <input id="password" name="password" type="password" placeholder="Your Password" required>
+            </p>
+
+            <p>
+            <label for="password2"> Confirm password</label>
+            <input id="password2" name="password2" type="password" placeholder="Your Password" required>
+            </p>
+
+            <button type="submit" name="registerButton">Sign Up</button>
+        </form>
+    </div>
+</body>
+</html>
