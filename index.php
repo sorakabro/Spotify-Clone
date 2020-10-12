b2e33b0a5a81ee4c50929c0b1c2b4a6a92825ef0
Benjamin Oldén <berntsson90@hotmail.com>
2020-10-12 16:08:24 +0200

    adding information to database

--- index.php
+++ index.php
@@ -14,8 +14,6 @@ else {
 
 ?>
 
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -32,155 +30,13 @@ else {
 
         <div id="topContainer">
 
-            <div id="navBarContainer">
-                <nav class="navBar">
-                    <a href="index.php" class="logo">
-                        <img src="assets/images/icons/logospotifyclone.png" alt="logo">
-                    </a>
-
-
-                    <div class="group">
-                        <div class="navItem">
-                            <a href="search.php" class="navItemLink">Search
-                            <img src="assets/images/icons/search.png" class="icon" alt="search icon">
-                            </a>
-                        </div>
-                    </div>
-
-                <div class="group">
-                        <div class="navItem">
-                            <a href="browse.php" class="navItemLink">Browse</a>
-                        </div>
-
-                        <div class="navItem">
-                            <a href="yourMusic.php" class="navItemLink">Your Music</a>
-                        </div>
-
-                        <div class="navItem">
-                            <a href="profile.php" class="navItemLink">Benjamin</a>
-                        </div>
-                 </div>
-
-
-
-
-                </nav>
-            </div>
-
+           <?php include("includes/navBarContainer.php"); ?>
 
         </div>
 
-
-            <div id="nowPlayingBarContainer">
-
-
-        <div id="nowPlayingBar">
-
-        <div id="nowPlayingLeft">
-            <div class="content">
-                <span class="albumLink">
-                    <img src="https://jooinn.com/images/square-4.jpg" alt="" class="albumArtwork">
-                </span>
-
-                <div class="trackInfo">
-                    <span class="trackName">
-                        <span>Hellow</span>
-                    </span>
-                    <span class="artistName">
-                        <span>Artist Name</span>
-                    </span>
-                </div>
-            </div>
-
-        </div>
-
-
-        <div id="nowPlayingCenter">
-
-        <div class="content playerControls">
-
-        <div class="buttons">
-
-            <button class="controlButton shuffle" title="Shuffle button">
-                <img src="assets/images/icons/shuffle.png" alt="Shuffle">
-            </button>
-
-            <button class="controlButton previous" title="Previous button">
-                <img src="assets/images/icons/previous.png" alt="Previous">
-            </button>
-
-            <button class="controlButton play" title="Play button">
-                <img src="assets/images/icons/play.png" alt="Play">
-            </button>
-
-            <button class="controlButton pause" title="Pause button" style="display:none;">
-                <img src="assets/images/icons/pause.png" alt="Pause">
-            </button>
-
-            <button class="controlButton next" title="Next button">
-                <img src="assets/images/icons/next.png" alt="Next">
-            </button>
-
-            <button class="controlButton repeat" title="Repeat button">
-                <img src="assets/images/icons/repeat.png" alt="Repeat">
-            </button>
-
-            
-
-        </div>
-
-        <div class="playBackBar">
-
-            <span class="progressTime current">0.00</span>
-
-            <div class="progressBar">
-                <div class="progressBarBg">
-                    <div class="progress"></div>
-                </div>
-            </div>
-
-            <span class="progressTime remaining">0.00</span>
-
-        </div>
-
-
-
-
-        </div>
-            
-        </div>
-
-
-        <div id="nowPlayingRight">
-
-        <div class="volumeBar">
-
-            <button class="controlButton volume" title="Volume Button">
-                <img src="assets/images/icons/volume.png" alt="Volume button">
-            </button>
-
-            <div class="progressBar">
-                <div class="progressBarBg">
-                    <div class="progress"></div>
-                </div>
-            </div>
-
-
-        </div>
-            
-        </div>
-
-
-
-        </div>
-
-        </div>
+        <?php include("includes/nowPlayingBar.php"); ?>
     </div>
     
-
-
-
-   
 
 </body>
 </html>
